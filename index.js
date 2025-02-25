880bfc1a6faa571e3dbfa0f0abd20657c9544f7f
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-02-26 00:00:30 +0600

    specific blog reading

--- index.js
+++ index.js
@@ -87,6 +87,13 @@ async function run() {
 
       res.send(result);
     });
+    // specific blog
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(query);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     //   await client.close();
