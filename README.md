6cc599edc48e4e3cef3de24e2787114fc3926724
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-02-25 21:40:41 +0600

    all blogs reading

---
+++ README.md
@@ -0,0 +1,49 @@
+# Server-2
+
+# For: web-practice-8 - BlogEng BD
+
+## Links:
+
+- **Web:** [https://mrirakib-web-practice-8.netlify.app/](https://mrirakib-web-practice-8.netlify.app/)
+- **Repo:** [https://github.com/mrirakib04/web-practice-8/](https://github.com/mrirakib04/web-practice-8/)
+
+## Features
+
+- **Blogs:**
+
+  - Fetch all blogs.
+  - Fetch a specific blog by ID.
+  - Fetch the latest blogs.
+  - Search blogs by text.
+  - Add a new blog.
+  - Update an existing blog.
+  - Sort blogs by description length.
+  - Fetch featured blogs based on description length.
+
+- **Comments:**
+
+  - Fetch all comments for a specific blog.
+  - Add new comments to a blog.
+
+- **Wishlist:**
+  - Fetch all wishlist items.
+  - Fetch wishlist items for a specific user.
+  - Add a new item to the wishlist.
+  - Remove an item from the wishlist.
+
+## Technologies Used
+
+- **Express.js**: Web framework for Node.js to handle HTTP requests.
+- **MongoDB**: NoSQL database to store blog posts, comments, and wishlist data.
+- **CORS**: Middleware to handle cross-origin requests.
+- **dotenv**: For managing environment variables such as MongoDB credentials and server configurations.
+- **JWT (JSON Web Token)**: For secure user authentication and authorization.
+- **cookie-parser**: Middleware to handle cookie parsing.
+
+## Conclusion
+
+Thank you for checking out my repository. If you have any feedback or suggestions, feel free to reach out!
+
+---
+
+Developed by **Md Rakibul Islam Rakib**
---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_ACCESS}@cluster0.bfqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: false,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+
+    // Connections
+    const database = client.db("blogeng_bd");
+    const blogsCollection = database.collection("blogs");
+    const commentsCollection = database.collection("comments");
+    const wishlistCollection = database.collection("wishlist");
+
+    // Reading
+    // all blogs
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("My HOT Server for BlogEng BD");
+});
+app.listen(port, () => {
+  console.log(`Server in port: ${port}`);
+});
